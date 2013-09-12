8b52b736a171575c2810edab60a75455e08beda7
RainMonster <the.rain.monster@gmail.com>
2013-09-11 20:05:43 -0700

    working multi-user tweets

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,25 +1,42 @@
 get '/' do
+
   erb :index
 end
 
+
 get '/sign_in' do
-  # the `request_token` method is defined in `app/helpers/oauth.rb`
-  redirect request_token.authorize_url
+  redirect request_token.authorize_url #### 2 - 4
 end
+
 
 get '/sign_out' do
   session.clear
   redirect '/'
 end
 
-get '/auth' do
+
+get '/auth' do #### 5
   # the `request_token` method is defined in `app/helpers/oauth.rb`
-  @access_token = request_token.get_access_token(:oauth_verifier => params[:oauth_verifier])
+  @access_token = request_token.get_access_token(:oauth_verifier => params[:oauth_verifier]) #### 6 & 7
+  p '$' * 200
   # our request token is only valid until we use it to get an access token, so let's delete it from our session
   session.delete(:request_token)
+  p @access_token.secret
+  @user = User.find_or_create_by_username(@access_token.params[:screen_name])
+  @user.oauth_token = @access_token.token
+  @user.oauth_secret = @access_token.secret
+  @user.save
+  session[:user_id] = @user.id
 
-  # at this point in the code is where you'll need to create your user account and store the access token
+  redirect '/'
+end
 
-  erb :index
-  
+
+post '/tweet_confirm' do
+  @twitter_user = Twitter::Client.new(
+    :oauth_token => current_user.oauth_token,
+    :oauth_token_secret => current_user.oauth_secret) 
+  @twitter_user.update(params[:tweet_input])
+
+  redirect '/'
 end
